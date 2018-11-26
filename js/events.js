6d26ad92ff0277beb5e1e41ee6546bd2579dea40
Chris Dalla Santa <chris.dallasanta@gmail.com>
2018-11-26 02:14:50 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -6,8 +6,8 @@ function getIt(){
 }
 
 function frameIt(){
-  $('img').on('load', function(){
-    console.log(img);
+  $('img').on('load', function(event){
+    console.log(event);
     $('img').classList.add("tasty");
   });
 }
